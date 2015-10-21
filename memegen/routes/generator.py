d5c174d4b783be5835016c09d963a37e29ab111e
Todd Wolfson <todd@twolfson.com>
2015-10-21 03:59:16 -0500

    Added generator page

---
+++ memegen/routes/generator.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, current_app as app, render_template
+from flask_api.decorators import set_renderers
+from flask_api.renderers import HTMLRenderer
+
+from ._common import url_for
+
+
+blueprint = Blueprint('generator', __name__, url_prefix="/generator")
+
+
+# TODO: Deduplicate with `overview#_gen`
+def _gen():
+    for template in sorted(app.template_service.all()):
+        path = template.sample_path
+        url = url_for("image.get", key=template.key, path=path, _external=True)
+        link = url_for("links.get", key=template.key, path=path)
+        yield {
+            'url': url,
+            'link': link
+        }
+
+
+@blueprint.route("")
+@set_renderers(HTMLRenderer)
+def get():
+    return render_template('generator.html', imgs=_gen())
