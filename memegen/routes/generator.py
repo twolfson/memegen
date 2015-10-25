4c5a06674916ab41a096d57716b6234815c6e91c
Todd Wolfson <todd@twolfson.com>
2015-10-25 16:35:15 -0500

    Moved to samples

--- memegen/routes/generator.py
+++ memegen/routes/generator.py
@@ -1,30 +1,17 @@
-from flask import Blueprint, current_app as app, render_template
+from flask import Blueprint, render_template
 from flask_api.decorators import set_renderers
 from flask_api.renderers import HTMLRenderer
 
-from ._common import url_for
+from ._common import samples
 
 
 blueprint = Blueprint('generator', __name__, url_prefix="/generator")
 
 
-def _gen():
-    for template in sorted(app.template_service.all()):
-        path = template.sample_path
-        url = url_for("image.get", key=template.key, path=path, _external=True)
-        link = url_for("links.get", key=template.key, path=path)
-        yield {
-            'key': template.key,
-            'name': template.name,
-            'url': url,
-            'link': link
-        }
-
-
 @blueprint.route("")
 @set_renderers(HTMLRenderer)
 def get():
-    imgs = [img for img in _gen()]
+    imgs = [img for img in samples()]
     # In the future, this could be provided via query param (similar to top/bottom)
     selected_img = imgs[0]
     selected_img['selected'] = True
